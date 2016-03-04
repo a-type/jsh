5726b6d82024c0d47e10991025a7963cde6ddae6
Grant Forrest <a-type@outlook.com>
2016-03-04 10:10:09 -0500

    Switch to AVA and XO

--- test/unit/helpers/testStream.js
+++ test/unit/helpers/testStream.js
@@ -1,5 +1,6 @@
 'use strict';
 const Duplex = require('stream').Duplex;
+const Bluebird = require('bluebird');
 
 export default class TestStream extends Duplex {
 	constructor () {
@@ -9,28 +10,38 @@ export default class TestStream extends Duplex {
 		this.input = [];
 	}
 
+	// manually inserts data into the input for testing purposes
 	send (data) {
 		if (!this.push(data)) {
 			this.input.push(new Buffer(data, 'utf-8'));
 		}
 	}
 
+	// shell will read input
 	_read () {
 		while (this.input.length) {
-			var chunk = this.input.shift();
+			const chunk = this.input.shift();
 			if (!this.push(chunk)) {
 				break;
 			}
 		}
 	}
 
+	// shell will write to output
 	_write (data, encoding, next) {
 		this.output.push(data.toString());
-
 		next();
 	}
 
+	// resets test stream state
 	clear () {
 		this.output = [];
+		this.input = [];
+	}
+
+	static wait (stream) {
+		return new Bluebird((resolve) => {
+			stream.on('data', resolve);
+		});
 	}
 }
--- test/unit/shell_spec.js
+++ test/unit/shell_spec.js
@@ -1,124 +1,96 @@
 'use strict';
 import BaseCommand from '../../lib/commands/baseCommand';
-import Shell from '../../lib/shell'
+import Shell from '../../lib/shell';
 import TestStream from './helpers/testStream';
-const streamSpec  = require('stream-spec');
-const Sinon       = require('sinon');
+const streamSpec = require('stream-spec');
+const Sinon = require('sinon');
 
-const expect = require('chai').expect;
+import test from 'ava';
 
-describe('the shell', () => {
-	it('is a valid duplex stream', () => {
-		streamSpec(new Shell())
-		.duplex({ strict : true })
-		.validateOnExit();
-	});
+test('the shell is a duplex stream', () => {
+	streamSpec(new Shell())
+	.duplex({ strict: true })
+	.validateOnExit();
+});
 
-	let shell;
-	let testStream;
+test('echoes input when told', (t) => {
+	const testStream = new TestStream();
+	const shell = new Shell();
+	testStream.pipe(shell);
+	shell.pipe(testStream);
 
-	before(() => {
-		testStream = new TestStream();
-		shell = new Shell();
+	const input = [ '\x09', 'a', 'b', 'c', '\x20' ];
+	input.forEach(testStream.send.bind(testStream));
 
-		testStream.pipe(shell);
-		shell.pipe(testStream);
-	});
-
-	describe('accepting input', () => {
-		describe('when configured to echo input', () => {
-			let input;
-
-			before(() => {
-				input = [ '\x09', 'a', 'b', 'c', '\x20' ];
-				input.forEach(testStream.send.bind(testStream));
-			});
-
-			after(() => {
-				testStream.clear();
-				shell._reset();
-			});
-
-			it('echoes all input back', () => {
-				input.forEach((data, idx) => {
-					expect(testStream.output[idx], 'output at ' + idx).to.equal(data);
-				});
-			});
-		});
-
-		describe('when configured to not echo input', () => {
-			let input;
-			let testStream;
-			let shell;
-
-			before(() => {
-				testStream = new TestStream();
-				shell = new Shell({ echoInput : false });
-
-				testStream.pipe(shell);
-				shell.pipe(testStream);
-				input = [ '\x09', 'a', 'b', 'c', '\x20' ];
-				input.forEach(testStream.send.bind(testStream));
-			});
-
-			after(() => {
-				testStream.clear();
-				shell._reset();
-			});
-
-			it('does not echo input back', () => {
-				expect(testStream.output.length, 'has output').to.equal(0);
-			});
+	return TestStream.wait(testStream)
+	.then(() => {
+		input.forEach((data, idx) => {
+			t.is(testStream.output[idx], data);
 		});
 	});
+});
 
-	describe('accepting command input', () => {
-		let input;
+test('does not echo input when not told', (t) => {
+	const testStream = new TestStream();
+	const shell = new Shell({ echoInput: false });
+	testStream.pipe(shell);
+	shell.pipe(testStream);
 
-		before(() => {
-			Sinon.spy(shell, 'createCommand');
-			Sinon.stub(BaseCommand.prototype, 'run');
+	const input = [ '\x09', 'a', 'b', 'c', '\x20' ];
+	input.forEach(testStream.send.bind(testStream));
 
-			input = [ 'l', 's', '\r' ];
-			input.forEach(testStream.send.bind(testStream));
-		});
-
-		after(() => {
-			shell.createCommand.restore();
-			BaseCommand.prototype.run.restore();
-			testStream.clear();
-			shell._reset();
-		});
-
-		it('runs the command', () => {
-			expect(shell.createCommand.callCount, 'command create').to.equal(1);
-			expect(shell.createCommand.calledWith('ls'), 'ls command create').to.be.true;
-			expect(BaseCommand.prototype.run.callCount, 'run called').to.equal(1);
-		});
+	return TestStream.wait(testStream)
+	.then(() => {
+		t.is(testStream.output.length, 0);
 	});
+});
 
-	describe('accepting command input with escaped characters', () => {
-		let input;
+test('runs commands', (t) => {
+	const testStream = new TestStream();
+	const shell = new Shell();
+	testStream.pipe(shell);
+	shell.pipe(testStream);
 
-		before(() => {
-			Sinon.spy(shell, 'createCommand');
-			Sinon.stub(BaseCommand.prototype, 'run');
+	const sandbox = Sinon.sandbox.create();
 
-			input = [ 'l', '\t', '\x01', 's', '\r' ];
-			input.forEach(testStream.send.bind(testStream));
-		});
+	const input = [ 'l', 's', '\r' ];
 
-		after(() => {
-			shell.createCommand.restore();
-			BaseCommand.prototype.run.restore();
-			testStream.clear();
-			shell._reset();
-		});
+	const mockCommand = Sinon.createStubInstance(BaseCommand);
+	sandbox.stub(shell, 'createCommand').returns(mockCommand);
 
-		it('runs the command without escaped input', () => {
-			expect(shell.createCommand.callCount, 'command create').to.equal(1);
-			expect(shell.createCommand.calledWith('ls'), 'ls command create').to.be.true;
-			expect(BaseCommand.prototype.run.callCount, 'run called').to.equal(1);
-		});
+	input.forEach(testStream.send.bind(testStream));
+
+	return TestStream.wait(testStream)
+	.then(() => {
+		t.is(shell.createCommand.callCount, 1);
+		t.is(shell.createCommand.calledWith('ls'), true);
+		t.is(mockCommand.run.callCount, 1);
+
+		sandbox.restore();
+	});
+});
+
+test('accepting command input with escaped characters', (t) => {
+	const testStream = new TestStream();
+	const shell = new Shell();
+	testStream.pipe(shell);
+	shell.pipe(testStream);
+
+	const sandbox = Sinon.sandbox.create();
+
+	const input = [ 'l', '\t', '\x01', 's', '\r' ];
+
+	sandbox.spy(shell, 'createCommand');
+	sandbox.stub(BaseCommand.prototype, 'run');
+
+	input.forEach(testStream.send.bind(testStream));
+
+	return TestStream.wait(testStream)
+	.then(() => {
+		t.is(shell.createCommand.callCount, 1);
+		t.is(shell.createCommand.calledWith('ls'), true);
+		t.is(BaseCommand.prototype.run.callCount, 1);
+
+		sandbox.restore();
 	});
 });
